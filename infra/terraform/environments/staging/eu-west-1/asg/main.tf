42dc2c234f1226d34de7c58ed9ce47ccf71748bc
Nurkhat Gabdollayev <nurkhat.gabdollayev@perimeter81.com>
2024-07-25 21:06:00 +0500

    Upodate infra pipeline steps

--- infra/terraform/environments/staging/eu-west-1/asg/main.tf
+++ infra/terraform/environments/staging/eu-west-1/asg/main.tf
@@ -1,21 +1,5 @@
-// Create Network resources
-module "network" {
-  source = "github.com/networkthor/terraform-module-network.git?ref=master"
-  environment     = var.environment
-  cidr_block      = var.cidr_block
-  private_subnets = var.private_subnets
-  public_subnets  = var.public_subnets
-  
-  tags            = {
-    Git_repo   = "terraform-resources"
-    Git_branch = "main"
-  }
-
-}
-
-
 module "elb" {
-  source = "github.com/networkthor/terraform-module-elb.git?ref=XYZ-6105-refactor-elb-module"
+  source = "git@github.com/networkthor/terraform-module-elb.git?ref=XYZ-6105-refactor-elb-module"
   environment        = var.environment
   public_subnet1_id  = module.network.public_subnets_id[0]
   public_subnet2_id  = module.network.public_subnets_id[1]
@@ -49,21 +33,4 @@ module "elb" {
   }
 
 }
-
-# module "autoscaling" {
-#   source = "github.com/networkthor/terraform-modules-autoscaling.git?ref=master"
-#   vpc_id                 = module.network.vpc_id
-#   elb_security_group_id  = module.elb.security_groups_id
-#   environment            = var.environment
-#   private_subnet1_id     = module.network.private_subnets_id[0]
-#   private_subnet2_id     = module.network.private_subnets_id[1]
-#   target_group_arn       = module.elb.target_group_arn
-#   ami_id                 = var.ami_id
-#   instance_type          = var.instance_type
-#   key_name               = var.key_name
-#   volume_size            = var.volume_size
-#   volume_type            = var.volume_type
-#   iam_instance_profile   = var.iam_instance_profile
-# }
-
 
